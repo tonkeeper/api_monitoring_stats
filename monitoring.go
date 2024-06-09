457606a71f6b14928a3b4c6c8d80b760fd41509e
Denis Subbotin <denis@subbot.in>
2024-06-09 14:36:24 +0300

    Update monitoring.go

--- monitoring.go
+++ monitoring.go
@@ -67,7 +67,7 @@ var (
 	}, []string{"bridge"})
 	metricBridgeLatencyHistogramVec = promauto.NewHistogramVec(prometheus.HistogramOpts{
 		Name:    "tonconnect_bridge_latency",
-		Buckets: []float64{0.01, 0.05, 0.1, 0.5, 1, 5, 10},
+		Buckets: []float64{0.01, 0.05, 0.1, 0.25, 0.5, 1,2, 5, 10},
 	}, []string{"bridge"})
 	metricBridgeReconnects = promauto.NewGaugeVec(prometheus.GaugeOpts{
 		Name: "tonconnect_bridge_reconnects",
